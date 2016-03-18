c8a6195b92a3881d56f31b2ee95dc36f54f987f0
Rishi Nag <rishi@ebi.ac.uk>
2016-03-18 13:52:44 +0000

    database basics and tidies in travis setup

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl/modules:$PWD/modules:$PWD/scripts:$PWD/ensembl-compara/modules:$PWD/ensembl-killlist/modules:$PWD/ensembl-hive/modules:$PWD/ensembl-io/modules:$PWD/bioperl-live:$PWD/bioperl-run/lib:$PWD/ensembl-test/modules
+export PERL5LIB=$PWD/bioperl-live:$PWD/ensembl/modules:$PWD/modules:$PWD/scripts:$PWD/ensembl-compara/modules:$PWD/ensembl-killlist/modules:$PWD/ensembl-hive/modules:$PWD/ensembl-io/modules:$PWD/bioperl-live:$PWD/bioperl-run/lib:$PWD/ensembl-test/modules
 
 export TEST_AUTHOR=$USER
 
@@ -8,11 +8,18 @@ export WORK_DIR=$PWD
 echo "Running test suite"
 echo "Using $PERL5LIB"
 
+if [ "$DB" = 'mysql' ]; then
+    ln -sf t/MultiTestDB.conf.mysql t/MultiTestDB.conf
+else
+    echo "Don't know about DB '$DB'"
+    exit 1;
+fi
+
 echo "COVERALLS value=$COVERALLS"
 
 if [ "$COVERALLS" = 'true' ]; then
   export PERL5LIB=$PERL5LIB:$PWD/ensembl-test/modules
-  PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl-test' perl $PWD/ensembl-test/scripts/runtests.pl -verbose $PWD/t $SKIP_TESTS
+  PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl-test' perl $PWD/ensembl-test/scripts/runtests.pl -verbose t $SKIP_TESTS
 else
   perl $PWD/ensembl-test/scripts/runtests.pl t $SKIP_TESTS
 fi
