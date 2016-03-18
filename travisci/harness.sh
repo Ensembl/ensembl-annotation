2e7116ef7643dedac02a3b920042a10fe9d29e5d
Rishi Nag <rishi@ebi.ac.uk>
2016-03-18 15:01:32 +0000

    remove duplicate PERL5LIB item

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PERL5LIB=$PWD/bioperl-live:$PWD/ensembl/modules:$PWD/modules:$PWD/scripts:$PWD/ensembl-compara/modules:$PWD/ensembl-killlist/modules:$PWD/ensembl-hive/modules:$PWD/ensembl-io/modules:$PWD/bioperl-live:$PWD/bioperl-run/lib:$PWD/ensembl-test/modules
+export PERL5LIB=$PWD/bioperl-live:$PWD/ensembl/modules:$PWD/modules:$PWD/scripts:$PWD/ensembl-compara/modules:$PWD/ensembl-killlist/modules:$PWD/ensembl-hive/modules:$PWD/ensembl-io/modules:$PWD/ensembl-test/modules
 
 export TEST_AUTHOR=$USER
 
